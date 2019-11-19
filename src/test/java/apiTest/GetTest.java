982ca1c45fb212245b7ad26d3d107f5d92d3798f
Dilya <deanabestom@gmail.com>
2019-11-18 20:45:22 -0800

    API Lesson 5

--- src/test/java/apiTest/GetTest.java
+++ src/test/java/apiTest/GetTest.java
@@ -1,4 +1,40 @@
 package apiTest;
 
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import models.BookResponse;
+import org.apache.http.client.utils.URIBuilder;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import io.restassured.response.Response;
+
+import java.net.URISyntaxException;
+
+import static io.restassured.RestAssured.given;
+
 public class GetTest {
+    @Test
+    public void testGet_getBookById() throws URISyntaxException {
+        URIBuilder url=new URIBuilder();
+        url.setScheme("http")
+                .setHost("http://http://booklibrarywebapidev.azurewebsites.net")
+                .setPath("api/books/id/240")
+                .setPort(80)
+                .build();
+        Response response = given().baseUri("http://booklibrarywebapidev.azurewebsites.net")
+        .basePath("api/books/id/240")
+        .header("Authorization", "Basic YWJjQHh5ei5jb206VGVzdHRlc3QxMjMh")
+        .contentType(ContentType.JSON)
+        .get()
+        .then()
+        .statusCode(200)
+        .extract()
+        .response();
+
+        JsonPath json = response.jsonPath();
+
+        BookResponse bookResponse=json.getObject("$",BookResponse.class);
+        Assert.assertTrue(true);
+
+    }
 }
