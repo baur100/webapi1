9fd6042f044314ec8316dcbbba809daecf3bd163
SvetlanaZap <zapalikova.svetlana@yahoo.com>
2019-11-19 18:03:17 -0800

    commit

--- src/test/java/apiTest/GetTest.java
+++ src/test/java/apiTest/GetTest.java
@@ -1,4 +1,19 @@
 package apiTest;
 
+import org.testng.annotations.Test;
+import io.restassured.response.Response;
+
+
 public class GetTest {
+
+    @Test
+            public void testGet_getBookById(){
+        Response responce = given()
+                .baseUrl("http://booklibrarywebapidev.azurewebsites.net")
+                .basePath("")
+                .header("Authorization", )
+
+    }
+
+
 }
