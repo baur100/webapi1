607b3887c8fcd96acaa5c35b14b8a2473a41afe5
Enkidoothis <enkidoothis@gmail.com>
2019-11-21 01:17:53 -0500

    Revert "Homework Done"
    This reverts commit 20e05808eb1b12aa6bf0eefda4c90beafe83cc40.

--- src/test/java/apiTest/GetTest.java
+++ src/test/java/apiTest/GetTest.java
@@ -1,71 +1,50 @@
 package apiTest;
 
+import io.restassured.http.ContentType;
+import org.apache.http.client.utils.URIBuilder;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import models.Book;
 import models.BookResponse;
-import models.GetAllBooksResponse;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.net.URISyntaxException;
-import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
-public class GetTest extends BaseTest{
+
+public class GetTest {
+
+
     @Test
     public void testGet_getABookById() throws URISyntaxException {
+        URIBuilder url = new URIBuilder();
+        url.setScheme("http")
+                .setHost("booklibrarywebapidev.azurewebsites.net")
+                .setPath("api/books/id/240")
+                .setPort(80);
+
         Response response =
                 given()
-                    .baseUri(baseUrl)
-                    .basePath("api/books/id/240")
-                    .headers(headers)
+                    .baseUri(url.toString())
+//                .basePath("api/books/id/240")
+                    .header("Authorization","Basic YWJjQHh5ei5jb206VGVzdHRlc3QxMjMh")
+                    .header("Content-Type","application/JSON")
+                    .contentType(ContentType.JSON)
                 .when()
                     .get()
                 .then()
+                    .statusCode(200)
                     .extract()
                     .response();
 
-        Assert.assertEquals(response.getStatusCode(),200);
+
+        
 
         JsonPath json = response.jsonPath();
         BookResponse bookResponse = json.getObject("$",BookResponse.class);
-        Book book = json.getObject("value", Book.class);
 
-        Assert.assertEquals(book.getId(),240);
-        Assert.assertEquals(book.getAuthor(),"XXstring");
-        Assert.assertEquals(book.getCondition(),"XXstring");
-        Assert.assertEquals(book.getGenre(),"XXstring");
-        Assert.assertEquals(book.getLabel(),"XXstring");
+        Assert.assertTrue(true);
 
-        Assert.assertEquals(bookResponse.getErrors().size(),0);
-        Assert.assertEquals(bookResponse.getValue().getId(),240);
-        Assert.assertEquals(bookResponse.getValue().getLabel(),"XXstring");
-        Assert.assertEquals(bookResponse.getValue().getAuthor(),"XXstring");
-        Assert.assertEquals(bookResponse.getValue().getGenre(),"XXstring");
-        Assert.assertEquals(bookResponse.getValue().getCondition(),"XXstring");
-    }
-
-    @Test
-    public void getAllBooks(){
-            Response response =
-                    given()
-                            .baseUri(baseUrl)
-                            .basePath("api/books/all")
-                            .headers(headers)
-                            .when()
-                            .get()
-                            .then()
-                            .extract()
-                            .response();
-            Assert.assertEquals(response.getStatusCode(),200);
-
-            JsonPath json = response.jsonPath();
-            GetAllBooksResponse getAllBookResponse = json.getObject("$", GetAllBooksResponse.class);
-            List<Book> booksList = getAllBookResponse.getValue();
-
-            boolean result = booksList.stream().anyMatch((s) -> s.getId()==5);
-            Assert.assertTrue(result);
     }
 }
