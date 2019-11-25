7ea9066b2abe6132af6cd9dadba8021300bf86db
AnnaM <Millerankatun@gmail.com>
2019-11-24 22:11:22 -0800

    AHW7

---
+++ src/test/java/models/BookUpdateResponse.java
@@ -0,0 +1,14 @@
+package models;
+import java.util.List;
+
+public class BookUpdateResponse {
+    public boolean value;
+    private List<String> errors;
+
+
+    public List<String> getErrors() {
+        return errors;
+    }
+
+
+}
