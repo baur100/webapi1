322088d0a5e91ca30b4c8e805b239ae8cceb21c7
jur4elo <jur4elo@yahoo.com>
2019-11-25 15:55:49 -0800

    yk15api_HomeWorkReady_Udate_DeleteNotExisting

--- src/test/java/models/BookUpdateResponse.java
+++ src/test/java/models/BookUpdateResponse.java
@@ -1,14 +1,13 @@
 package models;
 
+import com.sun.istack.Nullable;
+
 import java.util.List;
 
 public class BookUpdateResponse {
-    private boolean value;
+    @Nullable
+    public boolean value;
     private List<String> errors;
-
-    public boolean getValue() {
-        return value;
-    }
 
     public List<String> getErrors() {
         return errors;
