d9ea9c72b23c0318afc7013e5aeddb1fee910934
natalianam <natalia.nam1981@gmail.com>
2019-11-26 14:52:24 -0800

    lesson 9

--- src/test/java/apiTest/CreateUpdateDeleteTest.java
+++ src/test/java/apiTest/CreateUpdateDeleteTest.java
@@ -1,22 +1,53 @@
 package apiTest;
 
+
 import com.google.gson.Gson;
+import helpers.DataGenerator;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import models.Book;
 import models.BookCreateResponse;
 import models.BookDeleteResponse;
+import models.BookResponse;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import static helpers.DataGenerator.generateRandomBook;
 import static io.restassured.RestAssured.given;
 
 public class CreateUpdateDeleteTest extends BaseTest{
+    private Book book;
+
+
+    public void init(){
+        book = DataGenerator.generateRandomBook();
+        String payload = new Gson().toJson(book);
+
+        Response response =
+                given()
+                .baseUri(baseUrl)
+                .basePath("api/books")
+                .headers(headers)
+                .body(payload)
+                .post()
+                .then()
+                .extract()
+                .response();
+        var id = response.jsonPath().getObject("$",BookCreateResponse.class).getValue();
+        book.setId(id);
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        given().baseUri(baseUrl).basePath("api/books/id"+book.getId()).headers(headers).delete().then();
+
+    }
+
     @Test
     public void createBook(){
         //Arrange
-        Book book = generateRandomBook();
+        book = generateRandomBook();
         String payload = new Gson().toJson(book);
 
         //Act
@@ -38,14 +69,19 @@ public class CreateUpdateDeleteTest extends BaseTest{
         var bookCreateResponse = json.getObject("$",BookCreateResponse.class);
         Assert.assertEquals(bookCreateResponse.getErrors().size(),0);
         Assert.assertTrue(bookCreateResponse.getValue()>0);
+
+        book.setId((bookCreateResponse.getValue()));
+
         System.out.println(bookCreateResponse.getValue());
     }
 
     @Test
     public void deleteBook(){
+        init();
+
         Response response = given()
                 .baseUri(baseUrl)
-                .basePath("api/books/id/253")
+                .basePath("api/books/id/"+book.getId())
                 .headers(headers)
         .when()
                 .delete()
@@ -64,11 +100,62 @@ public class CreateUpdateDeleteTest extends BaseTest{
 
     @Test
     public void deleteNonExistingBook(){
+        Response response =
+                given().baseUri(baseUrl).basePath("api/books/id/260").headers(headers).when().delete().then().extract().response();
+        Assert.assertEquals(response.statusCode(), 404);
+
+        JsonPath json = response.jsonPath();
+        var bookDeleteResponse = json.getObject("$", BookDeleteResponse.class);
+        Assert.assertEquals(bookDeleteResponse.getErrors().size(), 1);
+        Assert.assertFalse(bookDeleteResponse.value);
+        Assert.assertEquals(bookDeleteResponse.getErrors().get(0), "The book record couldn't be found.");
 
     }
 
     @Test
     public void updateBook(){
+        init();
+
+        Book book1 = generateRandomBook();
+        String payload = new Gson().toJson(book1);
+
+        Response response =
+                given()
+                .baseUri(baseUrl)
+                .basePath("api/books/id/"+book.getId())
+                .headers(headers)
+                .body(payload)
+                .when()
+                .put()
+                .then()
+                .extract()
+                .response();
+
+        Assert.assertEquals(response.statusCode(), 200);
+
+        JsonPath json = response.jsonPath();
+        var bookUpdateResponse = json.getObject("$", BookDeleteResponse.class);
+        Assert.assertEquals(bookUpdateResponse.getErrors().size(),0);
+        Assert.assertNotNull(bookUpdateResponse.value);
+        Assert.assertTrue(bookUpdateResponse.value);
+
+    }
+    @Test
+    public void getBookById(){
+        init();
+        Response response =
+                given().baseUri(baseUrl).basePath("api/books/id/"+book.getId()).headers(headers)
+                .when().get().then().extract().response();
+
+        Assert.assertEquals(response.getStatusCode(), 200);
+
+        JsonPath json = response.jsonPath();
+        BookResponse bookResponse = json.getObject("$", BookResponse.class);
+
+        Assert.assertEquals(bookResponse.getValue().getLabel(),book.getLabel());
+        Assert.assertEquals(bookResponse.getValue().getAuthor(),book.getAuthor());
+        Assert.assertEquals(bookResponse.getValue().getGenre(),book.getGenre());
+        Assert.assertEquals(bookResponse.getValue().getCondition(),book.getCondition());
 
     }
 
