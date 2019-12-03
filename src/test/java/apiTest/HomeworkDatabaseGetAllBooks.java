0d5b4b235c08101caf782480f6e12a162234b454
TANIK STUDENT <tanik.student@gmail.com>
2019-12-02 20:17:08 -0800

    Homework Get all books from DB

---
+++ src/test/java/apiTest/HomeworkDatabaseGetAllBooks.java
@@ -0,0 +1,51 @@
+package apiTest;
+
+import helpers.DbAdapter;
+import models.Book;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeworkDatabaseGetAllBooks {
+
+    final private static String connectionUrl = "jdbc:sqlserver://aurorasqlserverdev.database.windows.net:1433;databaseName=lbrdev;user=aurorasqladmin;Password=mTDsj3BNe1J3!;sslProtocol=TLSv1";
+
+//    https://stackoverflow.com/questions/11983554/reading-data-from-database-and-storing-in-array-list-object/47794668
+
+    public static List<Book> getAllBooks() throws SQLException {
+            List<Book> list = new ArrayList<>();
+
+            Connection connection = DriverManager.getConnection(connectionUrl);
+            Statement statement = connection.createStatement();
+            String query = "SELECT *  FROM [dbo].[Books];";
+            ResultSet result = statement.executeQuery(query);
+
+
+
+            while (result.next()){
+
+                Book book = new Book();
+                book.setId(result.getInt("Id"));
+                book.setLabel(result.getString("Label"));
+                book.setAuthor(result.getString("Author"));
+                book.setGenre(result.getString("Genre"));
+                book.setCondition(result.getString("Condition"));
+
+                list.add(book);
+            }
+
+            return list;
+        }
+
+        @Test
+        public void callToDBAllBooks() throws SQLException {
+            var list = getAllBooks();
+            Assert.assertTrue(list.size()>0);
+        }
+
+    }
+
+
