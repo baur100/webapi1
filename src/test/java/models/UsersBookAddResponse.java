84fe75ea6100734ed6189201c28d7f832a751aad
Dilya <deanabestom@gmail.com>
2019-12-07 12:26:15 -0800

    API Lesson 16

---
+++ src/test/java/models/UsersBookAddResponse.java
@@ -0,0 +1,16 @@
+package models;
+
+import java.util.List;
+
+public class UsersBookAddResponse {
+    private int value;
+    private List<String> errors;
+
+    public int getValue() {
+        return value;
+    }
+
+    public List<String> getErrors() {
+        return errors;
+    }
+}
